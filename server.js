ac042df35210e7aee7bf8f030c1c696e944e936f
JPSauSara <jpsausara@users.noreply.github.com>
2017-06-18 00:09:46 -0500

    working: Scraping into a db with mongojs

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 
 // Dependencies:
 
+	const express = require("express");
+	const mongojs = require("mongojs");
+
+	// Require request and cheerio. This makes the scraping possible
 	// Snatches HTML from URLs
 	const request = require("request");
 	// Scrapes our HTML
 	const cheerio = require("cheerio");
+
 
 // First, tell the console what this server.js is doing
 console.log("\n******************************************\n" +
@@ -12,27 +17,89 @@ console.log("\n******************************************\n" +
             "from the US Soccer website:" +
             "\n******************************************\n");
 
-// Making a request call or ussoccer.com's homepage
-request("http://www.ussoccer.com/", (error,response,html) => {
+// Initialize Express
+	const app = express();
 
-	// Load the body of the HTML into cheerio
-	const $ = cheerio.load(html);
+// Database configuration
+	const databaseUrl = "scraperussf";
+	const collections = ["scrapedData"];
 
-	// Empty array to save our scraped data
-	const result = [];
+// Hook mongojs configuration to the db variable
+const db = mongojs(databaseUrl, collections);
+db.on("error", (error) => {
+	console.log("Database Error: ", error);
+});
 
-	// With cheerio, find each div-tag with the class "pod-summary"
-	$("div.pod-summary").each((i, element) => {
+// Main route (simple  Hello World Message)
+app.get("/", (req, res) => {
+	res.send("Hello World");
+});
 
-		const summary = $(element).find("span").text();
+// Retrieve data from the db
+app.get("/articles", (req, res) => {
+	db.scrapedData.find({}, (error, found) => {
+		// Throw any errors to the console
+		if (error) {
+			console.log (error);
+		}
+		// If there are no errors, send the data to the browser as a json
+		else {
+			res.json(found);
+		}
+	});
+});
 
-		result.push({
-			summary: summary,
+
+// Scrape data from one site and place it into the mongodb db
+app.get ("/scrape", (req, res) => {
+
+	// Making a request call or ussoccer.com's homepage
+	request("http://www.ussoccer.com/", (error,response,html) => {
+
+		// Load the body of the HTML into cheerio
+		const $ = cheerio.load(html);
+
+		// Empty array to save our scraped data
+		// const result = [];
+
+		// With cheerio, find each div-tag with the class "pod-summary"
+		$("div.pod-summary").each((i, element) => {
+
+			const summary = $(element).find("span").text();
+
+			// If this title element had both a title and a link
+			if (summary) {
+				// Save the data in the scrapedData db
+				db.scrapedData.save({
+					summary: summary,
+				}, (error, saved) => {
+					// If there's an error during this query
+					if (error) {
+						console.log(error);
+					}
+					else {
+						console.log(saved);
+					}
+				});
+			}
+
+			// result.push({
+			// 	summary: summary,
 		});
 	});
 
+	// This will send a "Scrape Complete" message to the browser
+	res.send("Scrape Complete");
+});
+
+// Listen on port 3000
+app.listen(3000, ()=> {
+	console.log("App running on port 3000!");
+});
+
+
 
 // After the program scans each div.pod-summary, log the result
-console.log(result);
+// console.log(result);
 
-});
+// });
